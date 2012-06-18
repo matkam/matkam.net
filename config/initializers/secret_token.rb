baab902b0ef75fe29d95d6d8560a78a167d1d6f0
Matt K <matkam@gmail.com>
2012-06-18 16:55:38 -0700

    Created initial app with RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatkamNet::Application.config.secret_token = 'ae71c4d509e3a092239429fd07e1ef3becf0c29bbecd0918380dee6b3fa2d6ce0d695d6fd4e2b84a42033e9ddd857e510ef1262903d1c2efb864d83738e346a8'
