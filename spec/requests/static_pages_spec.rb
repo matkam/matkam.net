5a53b21a2a02b4b418c19ea693813499c8655e24
Matt K <matkam@gmail.com>
2012-06-27 17:28:09 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Static pages" do
+ 
+
+  describe "Home page" do
+
+    it "should have the h1 'matkam.net'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'matkam.net')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "matkam.net | Home")
+    end
+
+  end
+
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "matkam.net | Help")
+    end
+
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text =>'About Us')
+    end
+    
+    it "should have the title 'About us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "matkam.net | About Us")
+    end
+
+  end
+
+
+end
