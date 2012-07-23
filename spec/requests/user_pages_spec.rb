71f31962860be800d9593a3475f2d34d349c113b
Matt K <matkam@gmail.com>
2012-07-23 14:48:51 -0700

    Added test for new user creation redirect

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,21 +32,31 @@ describe "UserPages" do
       describe "after submission" do
         before { click_button submit }
 
-        it { should have_selector('title', text: 'Sign Up') }
+        it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
       end
     end
 
     describe "with valid information" do
+      let(:email) { "user@example.com" }
+
       before do
         fill_in "Name",                   with: "Example User"
-        fill_in "Email",                  with: "user@example.com"
+        fill_in "Email",                  with: email
         fill_in "Password",               with: "foobar"
         fill_in "Password confirmation",  with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email(email) }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
