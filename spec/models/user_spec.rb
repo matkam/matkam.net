cf46cef4f2b375bfa7125ab95d42aa3c34fd1004
Matt K <matkam@gmail.com>
2012-08-22 19:55:35 -0700

    Added admin attribute to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,30 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
+  it { should_not be_admin }
+  
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it { should be_admin }
+  end
+  
+  describe "accessible attributes" do
+    it "should not allow access to 'admin'" do
+      expect do
+	User.new(name: @user.name, email: @user.email,
+	         password: @user.password, password_confirmation: @user.password,
+	         admin: true)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
   
   describe "when name is not present" do
     before { @user.name = " " }
