353b7f9de772313cf89e31053b7fc4727ef21d0c
Matt K <matkam@gmail.com>
2012-08-31 01:08:38 -0700

    Implemented microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -53,8 +53,7 @@ describe "Authentication" do
   describe "authoriztion" do
 
     describe "for non-signed-in- users" do
-      let(:user) { FactoryGirl.create(:user) }
-      
+      let(:user) { FactoryGirl.create(:user) } 
 
       describe "in the Users controller" do
 
@@ -95,6 +94,19 @@ describe "Authentication" do
 	      page.should have_selector('title', text:user.name)
 	    end
 	  end                           
+	end
+      end
+    
+      describe "in the Microposts controller" do
+	
+	describe "submitting to the create action" do
+	  before { post microposts_path }
+	  specify { response.should redirect_to(signin_path) }
+	end
+	
+	describe "submitting to the destroy action" do
+	  before { delete micropost_path(FactoryGirl.create(:micropost)) }
+	  specify { response.should redirect_to(signin_path) }
 	end
       end
     end
