10d0f7e4b1efe4d89d863fb731e99b0a755925e0
Mathew Kamkar <matkam@gmail.com>
2012-09-06 22:57:11 -0700

    Implemented following and followers pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -52,7 +52,7 @@ describe "Authentication" do
 
   describe "authoriztion" do
 
-    describe "for non-signed-in- users" do
+    describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) } 
 
       describe "in the Users controller" do
@@ -71,6 +71,16 @@ describe "Authentication" do
 	  before { visit users_path }
 	  it { should have_selector('title', text: 'Sign in') }
 	end
+	
+	describe "visiting the following page" do
+	  before { visit following_user_path(user) }
+	  it { should have_selector('title', text: 'Sign in') }
+	end
+	
+	describe "visiting the following page" do
+	  before { visit followers_user_path(user) }
+	  it { should have_selector('title', text: 'Sign in') }
+	end 
       end
 
       describe "when attempting to visit a protected page" do
